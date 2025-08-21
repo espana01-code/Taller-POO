feb44eb690ae9a97819a96c71ae76c6ad331bb8b
ALUMNO# <ALUMNO#@ALUMNO-02>
2025-08-21 10:41:32 -0500

    taller POO N6

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package javaapplication3;
 
-/**
- *
- * @author ALUMNO#
- */
+package javaapplication3;
 public class JavaApplication3 {
     public static void main(String[] args) { 
         double C = 21;
