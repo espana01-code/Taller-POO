bc020dcfb4deb114704aeb7916faeb72e52a53f7
ALUMNO# <ALUMNO#@ALUMNO-02>
2025-08-21 10:50:48 -0500

    taller POO N6

---
+++ src/javaapplication3/Taller1N6.java
@@ -0,0 +1,18 @@
+package taller1.n6;
+
+public class Taller1N6 {
+
+    public static void main(String[] args) {
+        int base, altura, area, perimetro;
+        base = 10;
+        altura = 20;
+        area = base * altura;
+      
+        perimetro = (base + altura) * 2;
+               
+        
+        System.out.println("El area del rectangulo es igual a:"+area);
+        System.out.println("El perimetro del rectangulo es igual a:"+perimetro);
+    }
+    
+}
