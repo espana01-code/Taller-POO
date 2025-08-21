a8235b17b53215fb389521d12fbc19cb4a26e518
ALUMNO# <ALUMNO#@ALUMNO-02>
2025-08-21 10:31:33 -0500

    taller POO N6

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -9,12 +9,13 @@ package javaapplication3;
  * @author ALUMNO#
  */
 public class JavaApplication3 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) { 
+        double C = 21;
+        double F = (C * 9/5) + 32;
+        System.out.println(C + " tus grados Celsius son " + F + " grados Fahrenheit.");
+        
+        F = 56;
+        C = (F - 32) * 5/9;
+        System.out.println(F + " tus grados Fahrenheit son " + C + " grados Celsius.");
     }
-    
 }
